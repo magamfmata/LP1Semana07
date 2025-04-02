86699db91c596cf0b85426b627d2d64c8eb5ab7a
Magali <magamayrmata@gmail.com>
2025-04-02 20:23:50 +0100

    Class program

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,7 +6,34 @@ namespace ColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Color color1 = new Color(255, 0, 0);
+            Sphere sphere = new Sphere(color1, 5.0f);
+
+            sphere.Throw();
+            sphere.Throw();
+            sphere.Throw();
+            sphere.Throw();
+            sphere.Pop();
+            sphere.Throw();
+
+            Console.WriteLine($"The sphere was thrown " +
+                $"{sphere.GetTimesThrown()} times.");
+            
+            redSphere.Throw();
+            blueSphere.Throw();
+            greenSphere.Throw();
+
+            
+            Console.WriteLine($"The blue sphere was thrown " +
+                $"{blueSphere.GetTimesThrown()} times.");
+            Console.WriteLine($"The green sphere was thrown " +
+                $"{greenSphere.GetTimesThrown()} times.");
+            Console.WriteLine($"The red sphere was thrown " +
+                $"{redSphere.GetTimesThrown()} times.");
+
+
+
+            
         }
     }
 }

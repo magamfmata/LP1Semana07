06d27949baa5297bb43363732732fa7affad2b62
Magali <magamayrmata@gmail.com>
2025-04-02 19:28:06 +0100

    Add Constructors

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -7,6 +7,28 @@ namespace ColorSpheres
 {
     public class Color
     {
-        
+        private byte red;
+
+        private byte green;
+
+        private byte blue;
+
+        private byte alpha;
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            _red = red;
+            _green = green;
+            _blue = blue;
+            _alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            _red = red;
+            _green = green;
+            _blue = blue;
+            _alpha = 255;
+        }
     }
 }
